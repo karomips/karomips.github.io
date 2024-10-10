1841b0be3ae4987a2bc88e4a632bdba5eb643002
asa. <157571714+karomips@users.noreply.github.com>
2024-10-10 23:35:26 +0800

    commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,27 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  kit: {
-    adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-    }),
-    paths: {
-      base: '/karomips.github.io', // this should match your GitHub repository name
-    },
-  },
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({ pages: "build", assets: "build", fallback: undefined, precompress: false, strict: true }),
+		
+		prerender: {
+			handleHttpError: ({ path, status }) => {
+				// Log or handle 404 errors gracefully during the build
+				if (status === 404) {
+					console.warn('Prerendering failed: ${status} for ${path}');
+				}
+			}
+		}
+	}
 };
+
+export default config;
